83fc2feac2b2c10a481209f69fff3850cd0b5674
mdashikulislam <ashik713832@gmail.com>
2020-07-05 18:46:11 +0600

    Initial laravel File

---
+++ app/Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Test
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Test newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Test newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Test query()
+ * @mixin \Eloquent
+ */
+class Test extends Model
+{
+
+
+}
