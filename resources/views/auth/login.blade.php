dbc6b6400712728839731ac5fe92ca7dcbb43052
mdashikulislam <ashik713832@gmail.com>
2020-07-09 16:54:39 +0600

    Customize Dashboard and include resource

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,7 +51,7 @@
                 @enderror
 
                 <div class="wrap-input100 validate-input" data-validate = "Password is required" style="margin-bottom:10px">
-                    <input class="input100" type="password" name="pass" placeholder="Password">
+                    <input class="input100" type="password" name="password" placeholder="Password">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
